c88461964a5d7ad64a5dda516173d66e6e44a126
Rahul Gaikwad <rahulgd77@yahoo.co.in>
2017-02-13 13:40:05 +0530

    Update webtest.sh

--- webtest.sh
+++ webtest.sh
@@ -6,3 +6,5 @@ systemctl disable firewalld
 yum install httpd -y
 systemctl restart httpd
 echo "This is just a test" > /opt/test.txt
+wget ftp://fr2.rpmfind.net/linux/dag/redhat/el7/en/x86_64/dag/RPMS/stress-1.0.2-1.el7.rf.x86_64.rpm -O /tmp/stress-1.0.2-1.el7.rf.x86_64.rpm
+rpm -ivh /tmp/stress-1.0.2-1.el7.rf.x86_64.rpm
