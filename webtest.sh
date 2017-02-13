0c607d08fd5946cf298d0b7535e37f843dae10be
Rahul Gaikwad <rahulgd77@yahoo.co.in>
2017-02-13 15:42:25 +0530

    Update webtest.sh

--- webtest.sh
+++ webtest.sh
@@ -8,3 +8,11 @@ systemctl restart httpd
 echo "This is just a test" > /opt/test.txt
 wget ftp://fr2.rpmfind.net/linux/dag/redhat/el7/en/x86_64/dag/RPMS/stress-1.0.2-1.el7.rf.x86_64.rpm -O /tmp/stress-1.0.2-1.el7.rf.x86_64.rpm
 rpm -ivh /tmp/stress-1.0.2-1.el7.rf.x86_64.rpm
+yum install tomcat -y  
+yum install tomcat-webapps tomcat-admin-webapps -y
+wget http://vm-confluence.eastus.cloudapp.azure.com:8081/repository/maven-snapshots/com/devOps/Credit_Card_App/1.0.0-SNAPSHOT/Credit_Card_App-1.0.0-20170213.095438-1.war -O /var/lib/tomcat/webapps/CreditCardApp.war
+chown tomcat:tomcat /var/lib/tomcat/webapps/CreditCardApp.war
+systemctl restart tomcat.service
+mv /etc/httpd/conf/httpd.conf /opt/
+wget https://raw.githubusercontent.com/rahulg17/demo_videos/master/httpd.conf -O /etc/httpd/conf/httpd.conf
+systemctl restart httpd
